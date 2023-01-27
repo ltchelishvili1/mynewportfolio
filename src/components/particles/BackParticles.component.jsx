c15a76c64039977040b3410ab2e8fe48a24803da
ltchelishvili1 <tchelishvililevani@gmail.com>
2023-01-27 15:48:07 +0400

    done

--- src/components/particles/BackParticles.component.jsx
+++ src/components/particles/BackParticles.component.jsx
@@ -17,53 +17,51 @@ const BackParticles = () => {
         id="tsparticles"
         init={particlesInit}
         loaded={particlesLoaded}
-        options={
-          {
-            background: {
-              color: "#000", // this sets a background color for the canvas
-            },
-            fullScreen: {
-              enable: true, // enabling this will make the canvas fill the entire screen, it's enabled by default
-              zIndex: -1, // this is the z-index value used when the fullScreen is enabled, it's 0 by default
-            },
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true, // enables the click event
-                  mode: "push", // adds the particles on click
-                },
-                onHover: {
-                  enable: true, // enables the hover event
-                  mode: "repulse", // make the particles run away from the cursor
-                },
+        options={{
+          background: {
+            color: "#000",
+          },
+          fullScreen: {
+            enable: true,
+            zIndex: -1,
+          },
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
               },
-              modes: {
-                push: {
-                  quantity: 10, // number of particles to add on click
-                },
-                repulse: {
-                  distance: 100, // distance of the particles from the cursor
-                },
+              onHover: {
+                enable: true,
+                mode: "repulse",
               },
             },
-            particles: {
-              links: {
-                enable: true, // enabling this will make particles linked together
-                distance: 200, // maximum distance for linking the particles
+            modes: {
+              push: {
+                quantity: 10,
               },
-              move: {
-                enable: true, // enabling this will make particles move in the canvas
-                speed: { min: 1, max: 5 }, // using a range in speed value will make particles move in a random speed between min/max values, each particles have its own value, it won't change in time by default
-              },
-              opacity: {
-                value: { min: 0.3, max: 0.7 }, // using a different opacity, to have some semitransparent effects
-              },
-              size: {
-                value: { min: 1, max: 3 }, // let's randomize the particles size a bit
+              repulse: {
+                distance: 100,
               },
             },
-          }
-        }
+          },
+          particles: {
+            links: {
+              enable: true,
+              distance: 100,
+            },
+            move: {
+              enable: true,
+              speed: { min: 1, max: 5 },
+            },
+            opacity: {
+              value: { min: 0.3, max: 0.7 },
+            },
+            size: {
+              value: { min: 1, max: 3 },
+            },
+          },
+        }}
       />
     </ParticleContainer>
   );
